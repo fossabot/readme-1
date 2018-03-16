4a26ba23ed943cf5384034b0f07386dcc6c1b872
micnncim <rerrkfuuul.85@gmail.com>
2018-03-16 18:43:24 +0900

    Initial commit

---
+++ cmd/readme.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/urfave/cli"
+)
+
+func Readme(c *cli.Context) error {
+	dir, err := os.Getwd()
+	if err != nil {
+
+	}
+
+	r := regexp.MustCompile(`.*/src/github.com/.+/.+`)
+	if !r.MatchString(dir) {
+		fmt.Println("Current directory must be GitHub repository.")
+		return err
+	}
+	el := strings.Split(dir, "/")
+	size := len(el)
+	user := el[size-2]
+	repo := el[size-1]
+
+	text := fmt.Sprintf(`# %s
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+Overview
+
+## :memo: Description
+
+Description
+
+***DEMO:***
+
+## :rocket: Features
+
+- Feature
+- ...
+
+## :zap: Requirement
+
+- Requirement
+- ...
+
+## :mag: Usage
+
+1. Usage
+1. ...
+
+## :package: Installation
+
+$ git clone https://github.com/%s/%s
+
+## :bulb: Anything Else
+
+Anything
+
+## :bust_in_silhouette: Author
+
+[@%s](https://twitter.com/%s)
+
+## :credit_card: License
+
+- [MIT](./LICENSE)`,
+		repo, user, repo, user, user)
+
+	readme := "README.md"
+	_, err = os.Create(readme)
+	if err != nil {
+		return err
+	}
+	ioutil.WriteFile(readme, []byte(text), 0666)
+	return err
+}
--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"os"
+
+	"github.com/micnncim/readme/cmd"
+	"github.com/urfave/cli"
+)
+
 func main() {
+	app := cli.NewApp()
+	app.Name = "readme"
+	app.Usage = "Generate README.md"
+	app.Action = cmd.Readme
+
+	app.Run(os.Args)
 }
